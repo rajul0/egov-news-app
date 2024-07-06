afad65591066d023975be0d9f983a792348f6b3a
rajul0 <sirajulilmi@gmail.com>
2024-07-06 23:33:27 +0700

    halaman detail berita

--- lib/halaman/beranda/component/spotlight_component.dart
+++ lib/halaman/beranda/component/spotlight_component.dart
@@ -1,3 +1,4 @@
+import 'package:egov_news_app/halaman/detail_berita/halaman_detail_berita.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -27,106 +28,122 @@ class _SpotlightComponentState extends State<SpotlightComponent> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180.0,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Color(0xFFBDBDBD),
-        borderRadius: BorderRadius.all(
-          Radius.circular(15.0),
-        ),
-      ),
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          CarouselSlider(
-            options: CarouselOptions(
-              viewportFraction: 1,
-              autoPlayCurve: Curves.fastOutSlowIn, // Animation curve
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HalamanDetailBerita(
+              gambarBerita: imageSliders[_selectedIndex],
+              judulBerita: judulBerita[_selectedIndex],
+              berita: "Ini isi berita",
+              penulis: penulis[_selectedIndex],
             ),
-            items: imageSliders.map((imagePath) {
-              int index = imageSliders.indexOf(imagePath);
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image(
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      image: AssetImage(imagePath),
-                    ),
-                    Container(
-                      color: Colors.black.withOpacity(0.4),
-                    ),
-                    Positioned(
-                      bottom: 30.0,
-                      left: 10.0,
-                      child: Container(
-                        width: 290.0,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              judulBerita[index],
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
+          ),
+        );
+      },
+      borderRadius: BorderRadius.circular(15.0),
+      child: Ink(
+        height: 180.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xFFBDBDBD),
+          borderRadius: BorderRadius.all(
+            Radius.circular(15.0),
+          ),
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                viewportFraction: 1,
+                autoPlayCurve: Curves.fastOutSlowIn, // Animation curve
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+              ),
+              items: imageSliders.map((imagePath) {
+                int index = imageSliders.indexOf(imagePath);
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image(
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        image: AssetImage(imagePath),
+                      ),
+                      Container(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                      Positioned(
+                        bottom: 30.0,
+                        left: 10.0,
+                        child: Container(
+                          width: 290.0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                judulBerita[index],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                softWrap: true,
                               ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              softWrap: true,
-                            ),
-                            Text(
-                              "by ${penulis[index]}",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10.0,
-                                fontWeight: FontWeight.bold,
+                              Text(
+                                "by ${penulis[index]}",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          Positioned(
-            bottom: 20.0,
-            right: 25.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: imageSliders.asMap().entries.map((entry) {
-                int index = entry.key;
-                return AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  width: index == _selectedIndex
-                      ? 30.0
-                      : 8.0, // Set width based on selected index
-                  height: 8.0,
-                  margin: EdgeInsets.symmetric(horizontal: 2.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                        index == _selectedIndex ? 15.0 : 50.0),
-                    shape: BoxShape.rectangle,
-                    color: index == _selectedIndex
-                        ? Color(0xFFFFFFFF)
-                        : Color(0xFFFFFF).withOpacity(0.2),
+                    ],
                   ),
                 );
               }).toList(),
             ),
-          )
-        ],
+            Positioned(
+              bottom: 20.0,
+              right: 25.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imageSliders.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  return AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    width: index == _selectedIndex
+                        ? 30.0
+                        : 8.0, // Set width based on selected index
+                    height: 8.0,
+                    margin: EdgeInsets.symmetric(horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                          index == _selectedIndex ? 15.0 : 50.0),
+                      shape: BoxShape.rectangle,
+                      color: index == _selectedIndex
+                          ? Color(0xFFFFFFFF)
+                          : Color(0xFFFFFF).withOpacity(0.2),
+                    ),
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/halaman/detail_berita/halaman_detail_berita.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class HalamanDetailBerita extends StatefulWidget {
+  final String gambarBerita;
+  final String judulBerita;
+  final String berita;
+  final String penulis;
+
+  const HalamanDetailBerita({
+    Key? key,
+    required this.gambarBerita,
+    required this.judulBerita,
+    required this.berita,
+    required this.penulis,
+  });
+
+  @override
+  State<HalamanDetailBerita> createState() => _HalamanDetailBeritaState();
+}
+
+class _HalamanDetailBeritaState extends State<HalamanDetailBerita> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 370.0,
+              decoration: BoxDecoration(
+                color: Color(0xFFBDBDBD),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20.0),
+                  bottomRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0)),
+                    child: Image.asset(
+                      width: double.infinity,
+                      widget.gambarBerita,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    top: 50.0,
+                    left: 24.0,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      borderRadius: BorderRadius.circular(50.0),
+                      child: Ink(
+                        child: Image.asset(
+                          "assets/gambar/back-logo.png",
+                          width: 54.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 24.0,
+                    left: 24.0,
+                    child: Container(
+                      width: screenWidth - 48.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.judulBerita,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: "Mulish",
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0),
+                            softWrap: true,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            "By ${widget.penulis}",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: "Mulish",
+                                fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                children: [
+                  Text("lorem ipsum"),
+                  Text("lorem ipsum"),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

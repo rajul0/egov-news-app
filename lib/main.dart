a4e9ae2139ad1b57a94d75d27ea102e5e6d360e3
rajul0 <sirajulilmi@gmail.com>
2025-06-20 21:11:43 +0700

    fix: change function type

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:SINERGI/halaman/halaman_beranda/halaman_beranda.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> main() async {
+void main() async {
   await dotenv.load();
   runApp(const NewsApp());
 }
